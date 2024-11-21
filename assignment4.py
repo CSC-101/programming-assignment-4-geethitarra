5e08d31d455b830d408ef8133a2bad2b43c713fd
Geethi Tarra <gtarra@calpoly.edu>
2024-11-21 01:45:59 -0800

    wokring

--- assignment4.py
+++ assignment4.py
@@ -33,6 +33,78 @@ def filter_greater_than(counties: list, field: str, cond: str, limit: float):
                     if education_value > limit:
                         result.append(county)
 
+def filter_less_than(counties: list, field: str, cond: str, limit: float):
+    result = []
+    if field == "Ethnicity":
+        for county in counties:
+            for i in county.ethnicities:
+                if cond in county.ethnicities:
+                    ethnicity_value = county.ethnicities[i][1]
+                    if ethnicity_value < limit:
+                        result.append(county)
+
+    elif field == "Income":
+        for county in counties:
+            if cond == county.income[1]:
+                income_value = county.income[1][1]
+                if income_value < limit:
+                    result.append(county)
+
+    elif field == "Education":
+        for county in counties:
+            for i in county.education:
+                if cond in county.education:
+                    education_value = county.education[i][1]
+                    if education_value < limit:
+                        result.append(county)
+
+
+def population (data:list):
+    total_pop = 0.0
+    for county in data_set:
+        pop = county.population["2014 Population"]
+        total_pop += pop
+    return total_pop
+    print(f'2014 population: {total_pop}')
+
+def  population_field (data: list, field: str, cond: str):
+
+    if field == "Ethnicity":
+        total = 0.0
+        for county in data:
+            for i in county.ethnicities:
+                if cond in county.ethnicities:
+                    ethnicities_value = county.ethnicities[i][1]
+                    county_total = (ethnicities_value / 100) * county.population["2014 Population"]
+                    total += county_total
+
+    elif field == "Income":
+        total = 0.0
+        for county in data:
+            if cond == county.income[1]:
+                income_value = county.income[1][1]
+                county_total = (income_value / 100) * county.population["2014 Population"]
+                total += county_total
+
+    elif field == "Education":
+        total = 0.0
+        for county in data:
+            for i in county.education:
+                if cond in county.education:
+                    education_value = county.education[i][1]
+                    county_total = (education_value / 100) * county.population["2014 Population"]
+                    total += county_total
+
+    print(f'2014 {field}.{cond} population: {total}')
+
+def percent_field(counties:list, field: str, cond: str):
+    sub_population = population_field(counties, field)
+    total_pop = population(counties)
+    percent = (sub_population / total_pop) * 100
+
+    print(f'2014 {field}.{cond} percentage: {percent}')
+    return percent
+
 try:
     #open the file
     operation = sys.argv[1]
@@ -71,3 +143,30 @@ try:
 
 
         if action == "filter-lt":
+            if "." in task[1]:
+                category = str(task[1].split('.')[0])
+                condition = str(task[1].split('.')[1])
+                if task[2]:
+                    bounds = float(task[2])
+                    filter_less_than(data_set, category, condition, bounds)
+                else:
+                    bounds = 0
+                    filter_less_than(data_set, category, condition, bounds)
+
+        if action == "population-total":
+            population(data_set)
+
+        if action == "population:":
+            category = str(task[1].split('.')[0])
+            condition = str(task[1].split('.')[1])
+            population_field(data_set, category, condition)
+
+
+        if action == "percent":
+            category = str(task[1].split('.')[0])
+            condition = str(task[1].split('.')[1])
+            percent_field(data_set, category, condition)
+
+
+except FileNotFoundError as e:
+    print("Error:", e)
